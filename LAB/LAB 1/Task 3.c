31081a15431236e7e6c84137c6ce710bd002ca04
Amber_Tariq <tariqamber002@gmail.com>
2025-10-25 13:16:46 +0500

    update

---
+++ LAB/LAB 1/Task 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+ {
+    int age = 18;
+    float marks = 87.5;
+    char grade = 'A';
+
+    printf("Age : %d\nMarks : %.2f\nGrade : %c\n",age,marks,grade);
+    return 0;
+}
